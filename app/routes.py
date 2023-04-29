20864a8d6b25dffe0ea3c086fa71cc922783c9ac
Ariel Higuera <purpl3m00ns@gmail.com>
2023-04-29 10:23:46 -0700

    complete 03 lesson

--- app/routes.py
+++ app/routes.py
@@ -18,16 +18,29 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
-def handle_books():
+@books_bp.route("", methods=["GET"])
+def read_all_books():
+    books = Book.query.all()
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description,
+        })
+    return jsonify(books_response)
+    
+@books_bp.route("", methods=["POST"])    
+def create_book():
     request_body = request.get_json()
     new_book = Book(title=request_body["title"],
                     description=request_body["description"])
-
     db.session.add(new_book)
     db.session.commit()
-
     return make_response(f"Book {new_book.title} successfully created", 201)
+
+
+
 
 #def validate_book(book_id):
 #    try:
