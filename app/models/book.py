bb58acaa7b301e4dd7df808cce1227527712f21f
Ariel Higuera <purpl3m00ns@gmail.com>
2023-05-01 19:53:14 -0700

    added helper function from_dict to book model

--- app/models/book.py
+++ app/models/book.py
@@ -10,3 +10,9 @@ class Book(db.Model):
                 "title": self.title,
                 "description": self.description
                 }
+    
+    @classmethod
+    def from_dict(cls, book_data):
+        new_book = Book(title=book_data["title"],
+                        description=book_data["description"]) # how can I make a new instance of book inside a method that is inside the class book?
+        return new_book
