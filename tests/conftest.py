bd6e50f336e97a1938ad8378eb023121d17fefc9
Ariel Higuera <purpl3m00ns@gmail.com>
2023-05-01 18:48:49 -0700

    add conftest and update test routes to include reading a book by id

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,4 +24,12 @@ def app():
 def client(app):
     return app.test_client()
 
-
+@pytest.fixture
+def two_saved_books(app):
+    ocean_book = Book(title="Ocean Book",
+                      description="watr 4evr")
+    mountain_book = Book(title="Mountain Book",
+                         description = "i luv 2 climb rocks")
+    
+    db.session.add_all([ocean_book, mountain_book])
+    db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,3 +6,26 @@ def test_get_all_books_with_no_records(client):
     # Assert
     assert response.status_code == 200
     assert response_body == []
+
+def test_get_one_book(client):
+    response = client("/books/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == {
+        "id": "",
+        "title": "",
+        "description": ""
+    }
+
+def test_get_book_by_id(client, two_saved_books): # where is twosavedbooks getting called?
+    response = client.get("/books/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body = {
+        "id": 1,
+        "book": "Ocean Book",
+        "description": "watr 4evr"
+        }
+
